17a44fdd11b142291f68cec17d148dbc3f2873a4
Sai Ganesh Ratnala <125180139+saiganesh1976@users.noreply.github.com>
2023-07-26 12:10:50 +0530

    Add files via upload

---
+++ sliders.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'meeku maathrame cheptha',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: './images/slider 2.PNG'
+    },
+    {
+        name: 'Roar of the lion',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: './images/slider 1.PNG'
+    },
+    {
+        name: 'RRR',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: './images/slider 3.PNG'
+    },
+    {
+        name: 'IND vs WI ',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: './images/slider 4.PNG'
+    },
+    {
+        name: 'ICC Men\'s T20 WorldCup' ,
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: './images/slider 5.PNG'
+    }
+]
